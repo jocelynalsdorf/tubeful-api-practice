f81c9a8c1df6ef68b14adad3fc1d2f1a6593a483
jocelynalsdorf <jfrodsla@gmail.com>
2014-11-30 13:03:16 +0700

    add thumbnails styling and change to prepend so fresh search results show due to max of 8

--- apps/app.js
+++ apps/app.js
@@ -1,11 +1,10 @@
-
 $(function() {
 
 
   $("#search-form").submit(function(event) {
     event.preventDefault();
     var searchTerm = $('#query').val();
-    search(searchTerm);
+    search(searchTerm)
   });
 
 
@@ -15,13 +14,24 @@ $(function() {
       key: "AIzaSyCdfadHd-MS-pAihYaStxo9-g6k9EMeYb0",
       part: "snippet",
       type:'video',
-      q: term
-    };
+      q: term,
+      maxResults: 8
+
+    }
 
     $.getJSON("https://www.googleapis.com/youtube/v3/search", query, function(data) {
-      console.log(data.items);
+      var resultsArray = data.items;
+      var arrayLength = resultsArray.length;
+      for(var i = 0; i <arrayLength; i++) {
+         console.log(resultsArray[i].id.videoId);
+          $("#search-results").prepend("<a href=https://www.youtube.com/watch?v="+
+          resultsArray[i].id.videoId+"><img src="+resultsArray[i].snippet.thumbnails.default.url+"></a>");
+      };
+
+
+     
     });
 
-  }
+  };
 
 });
